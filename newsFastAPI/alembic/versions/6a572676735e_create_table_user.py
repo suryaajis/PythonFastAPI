17c4c93448e8f350bf7dff03b778165552c97c19
Surya Aji S <suryaajis330@gmail.com>
2024-03-31 22:44:37 +0700

    init alembic for revision

---
+++ newsFastAPI/alembic/versions/6a572676735e_create_table_user.py
@@ -0,0 +1,28 @@
+"""create table user
+
+Revision ID: 6a572676735e
+Revises: 
+Create Date: 2024-03-31 21:50:51.327130
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '6a572676735e'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table("users", sa.Column("id", sa.Integer(), primary_key=True, nullable=False, index=True) , sa.Column("email", sa.String(), nullable=False, unique=True), sa.Column("username", sa.String(), nullable=False), sa.Column("password", sa.String(), nullable=False))
+    pass
+
+
+def downgrade() -> None:
+    op.drop_table("users")
+    pass
